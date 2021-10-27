ea35ef2def875ff0bea00e375d1b72db72a80380
Dionis <63014993+DioniS1902@users.noreply.github.com>
2021-10-28 01:18:38 +0300

    Update lab2.sql

--- lab2.sql
+++ lab2.sql
@@ -16,6 +16,13 @@ FROM   product
        LEFT JOIN pc
               ON pc.model = product.model;
 
+SELECT maker
+FROM product LEFT JOIN pc ON  pc.model = product.model
+WHERE type="PC"
+GROUP BY maker
+	HAVING COUNT(DISTINCT pc.model) BETWEEN 1 AND COUNT(DISTINCT product.model) - 1; 
+ 
+
 SELECT DISTINCT maker
 FROM   product
 WHERE  product.type = 'Printer'
